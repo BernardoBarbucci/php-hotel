f7d2545f30a75c9312a881993522a807421221cc
BernardoBarbucci <bernardobarbucci@gmail.com>
2024-01-15 17:14:05 +0100

    risolto problema link di bootstrap, aggiustate grandezze e col, aggiunti un paio di colori ed un ultimo hotel per rendere il tutto piu bello esteticamente

--- hotel.php
+++ hotel.php
@@ -37,6 +37,13 @@ $hotels = [
         'vote' => 2,
         'distance_to_center' => 50
     ],
+    [
+        'name' => 'Hotel Siena',
+        'description' => 'Grande e meraviglioso hotel',
+        'parking' => true,
+        'vote' => 10,
+        'distance_to_center' => 0
+    ],
 
 ];
 
@@ -49,19 +56,19 @@ $hotels = [
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>php Hotels</title>
-    <!-- BOOTSTRAP -->
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous">
-    </script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js" integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous">
+    <!-- bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous">
     </script>
 
-    <!-- STYLES -->
     <style>
-        body {
-            margin: 0;
-            padding: 0;
-            box-sizing: border-box;
-            background: linear-gradient(90deg, #000, #790000fa);
+        main {
+            height: 100vh;
+            background: linear-gradient(180deg, #311f13, #a87e62);
+        }
+
+        .card {
+            background-color: #f4decb;
         }
 
         h1 {
@@ -76,15 +83,15 @@ $hotels = [
 </head>
 
 <body>
-    <main>
+    <main class="d-flex align-items-center">
         <section class="container">
             <div class="row d-flex justify-content-center">
-                <div class="card col-lg-6 border">
-                    <div class="row text-white">
+                <div class="card offset-1 col-10 border">
+                    <div class="row">
                         <!-- foreach per gli hotel -->
                         <?php foreach ($hotels as $hotel) : ?>
-                            <div class="col-lg-3 col-md-4 col-sm-6">
-                                <div class="card">
+                            <div class="col-4">
+                                <div class="card mb-3 bg-primary">
                                     <h1> <?php echo $hotel['name']; ?> </h1>
                                     <p> <?php echo $hotel['description']; ?> </p>
                                     <p> <?php echo $hotel['vote']; ?> </p>
