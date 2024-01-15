60042b8daccb390d0debbbb3fce9fd44108c37d4
BernardoBarbucci <bernardobarbucci@gmail.com>
2024-01-15 17:54:56 +0100

    struttura completata. da aggiungere form per filtrare gli hotel con i parcheggi

--- hotel.php
+++ hotel.php
@@ -57,47 +57,62 @@ $hotels = [
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>php Hotels</title>
     <!-- bootstrap -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous">
     </script>
 
     <style>
-        main {
-            height: 100vh;
-            background: linear-gradient(180deg, #311f13, #a87e62);
-        }
+    main {
+        height: 100vh;
+        background: linear-gradient(180deg, #311f13, #a87e62);
+    }
 
-        .card {
-            background-color: #f4decb;
-        }
+    .card {
+        background-color: #f4decb;
+        min-height: 15rem;
+    }
 
-        h1 {
-            color: white;
-        }
+    #custom_card {
+        background-color: #3a2317;
+        transition: linear .3s;
+    }
 
-        p {
-            color: white;
-        }
+    #custom_card:hover {
+        background-color: #573721;
+        border: 2px solid white;
+    }
+
+    h1 {
+        color: white;
+        font-size: 1.4rem;
+    }
+
+    p {
+        color: white;
+    }
     </style>
 
 </head>
 
 <body>
     <main class="d-flex align-items-center">
-        <section class="container">
+        <section class="container d-flex flex-column align-items-center">
+            <h1 class="mb-5">PHP Hotels</h1>
             <div class="row d-flex justify-content-center">
-                <div class="card offset-1 col-10 border">
-                    <div class="row">
+                <div class="card col-11 border">
+                    <div class="row pt-3">
                         <!-- foreach per gli hotel -->
                         <?php foreach ($hotels as $hotel) : ?>
-                            <div class="col-4">
-                                <div class="card mb-3 bg-primary">
-                                    <h1> <?php echo $hotel['name']; ?> </h1>
-                                    <p> <?php echo $hotel['description']; ?> </p>
-                                    <p> <?php echo $hotel['vote']; ?> </p>
-                                    <p> <?php echo $hotel['distance_to_center']; ?> </p>
-                                </div>
+                        <div class="col-4">
+                            <div class="card mb-3 p-2" id="custom_card">
+                                <h1> <?php echo $hotel['name']; ?> </h1>
+                                <p> <?php echo $hotel['description']; ?> </p>
+                                <p> Voto: <?php echo $hotel['vote']; ?> </p>
+                                <p>Distanza dal centro: <?php echo $hotel['distance_to_center']; ?>km </p>
                             </div>
+                        </div>
                         <?php endforeach; ?>
                     </div>
                 </div>
